3e61c0088633af34ee54d347cd40e1e2a73d4510
joybully <joybully@khu.ac.kr>
2024-01-05 02:38:02 +0900

    feat: Add component scan, Filter

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    //memberRepository타입에 맞는 객체를 찾아와서 자동으로 연결해 의존관계를 주입한다.
+    @Autowired //ac.getBean(MemberRepository.class)와 비슷하게 동작한다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
